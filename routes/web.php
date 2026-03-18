5c63da6271c7eaaa6cd51827addf9830a115deb1
bogdunchik <bogdun0713@gmail.com>
2026-03-19 03:59:35 +0600

    pr1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('hiii');
 });
-
+// Задача 1
+Route::get('/test/', function () {
+    return 'привееет!';
+});
+// Задача 2
+Route::get('/dir/test/', function () {
+    return '!!!!';
+});
+// Задача 3
+Route::get('/user/{id}', function ($id) {
+    return $id;
+});
+// Задача 4
+Route::get('/user/{name}', function ($name) {
+    return 'Вася' . $name;
+});
+// Задача 5
+Route::get('/sum/{num1}/{num2}', function ($num1, $num2) {
+    return $num1 + $num2;
+});
+// Задача 6
+Route::get('/user/show-{id}', function ($id) {
+    return $id;
+});
+// Задача 7
+Route::get('/user/{id?}', function ($id = 0) {
+    return $id;
+});
