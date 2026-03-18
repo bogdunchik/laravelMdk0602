ed32f1c91b2ba667afa33ce2730ad711842b92cf
bogdunchik <bogdun0713@gmail.com>
2026-03-18 22:41:25 +0600

    Update web.php
    test update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+<?php
 Route::get('/', function () {
     return view('hii');
 });
+// Задача 1
+Route::get('/test/', function () {
+    return '!';
+});
+// Задача 2
+Route::get('/dir/test/', function () {
+    return '!!!!!!!!!';
+});
+// Задача 3
+Route::get('/user/{id}', function ($id) {
+    return $id;
+});
+// Задача 4
+Route::get('/user/{name}', function ($name) {
+    return 'Вася' . $name;
+});
+// Задача 5
+Route::get('/sum/{num1}/{num2}', function ($num1, $num2) {
+    return $num1 + $num2;
+});
+// Задача 6
+Route::get('/user/show-{id}', function ($id) {
+    return $id;
+});
+// Задача 7
+Route::get('/user/{id?}', function ($id = 0) {
+    return $id;
+});
+
+
